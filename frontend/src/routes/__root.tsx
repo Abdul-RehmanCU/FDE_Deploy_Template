d21d4ce1cf35c13ede650b43bed2b8eab16c0891
Abdul <sonubhai883@gmail.com>
2026-09-22 04:17:33 -0400

    fix(frontend): remove inherited footer and debug overlays

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,4 @@
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 import ErrorComponent from "@/components/Common/ErrorComponent"
 import NotFound from "@/components/Common/NotFound"
 
@@ -15,8 +13,6 @@ export const Route = createRootRoute({
       </a>
       <HeadContent />
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
-      <ReactQueryDevtools initialIsOpen={false} />
     </>
   ),
   notFoundComponent: () => <NotFound />,
